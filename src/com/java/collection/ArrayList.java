1d97291c7f7b4bae806eb5b0b77fc06e75463978
keencho <seyoung050412@gmail.com>
2020-10-01 14:38:40 +0900

    commit

--- src/com/java/collection/ArrayList.java
+++ src/com/java/collection/ArrayList.java
@@ -3,6 +3,7 @@ package com.java.collection;
 import java.util.List;
 
 public class ArrayList {
+    // commit test
     public static void main(String[] args) {
         List<String> test = new java.util.ArrayList<>();
 
