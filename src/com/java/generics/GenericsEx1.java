dd0a3a795c0520d48b60ccc2fac2d3b39ab8d134
keencho <seyoung050412@gmail.com>
2020-10-14 21:39:02 +0900

    restrict generics

--- src/com/java/generics/GenericsEx1.java
+++ src/com/java/generics/GenericsEx1.java
@@ -2,16 +2,20 @@ package com.java.generics;
 
 import java.util.ArrayList;
 
-class Fruit                 { public String toString() { return "Fruit"; }}
+class Fruit implements Eatable {
+    public String toString() { return "Fruit"; }
+}
 class Apple extends Fruit   { public String toString() { return "Apple"; }}
 class Grape extends Fruit   { public String toString() { return "Grape"; }}
 class Toy                   { public String toString() { return "Toy"; }}
 
+interface Eatable{}
+
 public class GenericsEx1 {
     public static void main(String[] args) {
-        Box<Fruit> fruitBox = new Box<Fruit>();
-        Box<Apple> appleBox = new Box<Apple>();
-        Box<Toy> toyBox = new Box<Toy>();
+        FruitBox<Fruit> fruitBox = new FruitBox<Fruit>();
+        FruitBox<Apple> appleBox = new FruitBox<Apple>();
+//        FruitBox<Toy> toyBox = new FruitBox<Toy>();
 //        Box<Grape> grapeBox = new Box<Apple>();
 
         fruitBox.add(new Fruit());
@@ -20,14 +24,16 @@ public class GenericsEx1 {
         appleBox.add(new Apple());
 //        appleBox.add(new Toy());
 
-        toyBox.add(new Toy());
+//        toyBox.add(new Toy());
 //        toyBox.add(new Fruit());
 
         System.out.println(fruitBox);
         System.out.println(appleBox);
-        System.out.println(toyBox);
+//        System.out.println(toyBox);
     }
 }
+
+class FruitBox<T extends Fruit & Eatable> extends Box<T> {}
 
 class Box<T> {
     ArrayList<T> list = new ArrayList<>();
