02e4f38107e29ffa34fde96fb67f6c987404a3b3
keencho <seyoung050412@gmail.com>
2020-12-14 20:30:06 +0900

    thread test

--- src/com/java/thread/ThreadTest.java
+++ src/com/java/thread/ThreadTest.java
@@ -1,7 +1,9 @@
 package com.java.thread;
 
+import java.util.concurrent.ForkJoinPool;
+
 public class ThreadTest {
     public static void main(String[] args) {
-
+        ForkJoinPool t = new ForkJoinPool(10);
     }
 }
