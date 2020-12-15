2167d34383995c0456dcd9b43141be52dd1b0b23
keencho <seyoung050412@gmail.com>
2020-12-15 21:11:00 +0900

    thread test

--- src/com/java/thread/ThreadTest.java
+++ src/com/java/thread/ThreadTest.java
@@ -1,9 +1,15 @@
 package com.java.thread;
 
+import java.util.Date;
 import java.util.concurrent.ForkJoinPool;
 
 public class ThreadTest {
     public static void main(String[] args) {
         ForkJoinPool t = new ForkJoinPool(10);
+        t.execute(() -> test());
+    }
+
+    private static void test() {
+        System.out.println(new Date());
     }
 }
