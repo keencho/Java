406f12253405422fe4217c29b4f4cc8972153fff
keencho <seyoung050412@gmail.com>
2020-12-19 12:12:40 +0900

    thread test

--- src/com/java/thread/ThreadTest.java
+++ src/com/java/thread/ThreadTest.java
@@ -7,7 +7,7 @@ public class ThreadTest {
     public static void main(String[] args) {
         ForkJoinPool t = new ForkJoinPool(10);
         t.execute(() -> test());
-        System.out.println("today is rest day 3 and tomorrow will be a rest day toooooo ");
+        System.out.println("today is rest day 4 and tomorrow will studyyyyyy!! ");
     }
 
     private static void test() {
