0d365b57716dd26bc8eeeeeb3a7cc447e5c8de07
keencho <seyoung050412@gmail.com>
2020-12-22 21:38:45 +0900

    thread test

---
+++ src/com/java/thread/MyThread.java
@@ -0,0 +1,23 @@
+package com.java.thread;
+
+import java.util.Date;
+
+public class MyThread implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0 ; i < 5; i ++) {
+            System.out.println(Thread.currentThread().getName() + i);
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Runnable myThread = new MyThread();
+
+        Thread t1 = new Thread(myThread);
+
+        t1.start();
+        // t1.start(); IllegalThreadStateException - 한번 종료된 쓰레드는 재시작 불가. 만약 하고자 한다면 새로운 쓰레드를 생성해야함
+    }
+}
