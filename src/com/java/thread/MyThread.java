06eee683f0769e476065dcbdea8440261e8a5951
keencho <seyoung050412@gmail.com>
2020-12-25 19:53:14 +0900

    thread test with merry christmas

--- src/com/java/thread/MyThread.java
+++ src/com/java/thread/MyThread.java
@@ -7,7 +7,7 @@ public class MyThread implements Runnable{
     public void run() {
         for (int i = 0 ; i < 5; i ++) {
             System.out.println(Thread.currentThread().getName() + i);
-            System.out.println("christmas...");
+            System.out.println("merry christmas...");
         }
     }
 }
